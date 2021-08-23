5562ae9c4ce0337dbdbae24220031e5b97f0bf91
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2021-08-23 08:32:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Grid, Container } from "@material-ui/core";
+import "normalize.css";
+import "./App.css";
 
-function App() {
+import Scores from "./component/Scores/Scores.jsx";
+
+const url = "https://5f5dec4e8b224700167c53f1.mockapi.io/api/v1/results";
+
+const App = () => {
+  const [scores, setScores] = useState([]);
+  const [expanded, setExpanded] = useState(null);
+
+  console.log(scores);
+
+  useEffect(() => {
+    const getScores = async () => {
+      try {
+        const { data } = await axios.get(url);
+        setScores(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getScores();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Grid container spacing={2}>
+          {scores.map(({ id, home, away, data, image, referee, stadium }) => (
+            <Scores
+              key={id}
+              id={id}
+              expanded={expanded === id}
+              setExpanded={setExpanded}
+              home={home}
+              away={away}
+              data={data}
+              image={image}
+              referee={referee}
+              stadium={stadium}
+            />
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/component/Scores/Scores.jsx
@@ -0,0 +1,156 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { lime } from "@material-ui/core/colors";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Grid } from "@material-ui/core";
+import dayjs from "dayjs";
+import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 700,
+    background: "linear-gradient(25deg, #d0fdfd, #f7c0c0)",
+    "&:hover": {
+      background: "linear-gradient(25deg, #a5f8f8, #fca8a8)",
+      backdropFilter: "blur(3px)",
+      WebkitBackdropFilter: "blur(3px)",
+      boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+      border: "1px solid rgba( 255, 255, 255, 0.20 )",
+      borderRadius: "7px",
+    },
+  },
+
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: lime[50],
+  },
+  CardContent: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  cardItems: {
+    display: "flex",
+    alignItems: "center",
+  },
+  name: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}));
+
+const Scores = ({
+  id,
+  image,
+  home,
+  away,
+  stadium,
+  referee,
+  date,
+  expanded,
+  setExpanded,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Grid item xs={12} sm={6} md={6} lg={6}>
+      <Card className={classes.root}>
+        <Typography>{stadium}</Typography>
+        <CardContent>
+          <div className={classes.CardContent}>
+            <div className={classes.cardItems}>
+              <CardHeader
+                avatar={
+                  <Avatar aria-label="recipe" className={classes.avatar}>
+                    <img
+                      src={home.logoUrl}
+                      height="38px"
+                      width="38px"
+                      alt="club-logo"
+                    />
+                  </Avatar>
+                }
+              />
+              <Typography style={{ fontWeight: "700" }}>{home.name}</Typography>
+            </div>
+            <div className={classes.scores}>
+              <Typography style={{ fontWeight: "700" }}>
+                {home.score}
+              </Typography>
+            </div>
+          </div>
+          <div>
+            <div className={classes.CardContent}>
+              <div className={classes.cardItems}>
+                <CardHeader
+                  avatar={
+                    <Avatar aria-label="club-logo" className={classes.avatar}>
+                      <img
+                        src={away.logoUrl}
+                        height="37px"
+                        width="37px"
+                        alt="club-logo"
+                      />
+                    </Avatar>
+                  }
+                />
+                <Typography style={{ fontWeight: "700" }}>
+                  {away.name}
+                </Typography>
+              </div>
+              <div className={classes.scores}>
+                <Typography style={{ fontWeight: "700" }}>
+                  {away.score}
+                </Typography>
+              </div>
+            </div>
+          </div>
+        </CardContent>
+
+        <CardActions disableSpacing>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={() => setExpanded(expanded ? null : id)}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Method:</Typography>
+            <CalendarTodayIcon />
+            <Typography gutterBottom>
+              {dayjs(date).format("ddd, D MMM - h:mmA")}
+            </Typography>
+            <Typography>{referee}</Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </Grid>
+  );
+};
+
+export default Scores;
